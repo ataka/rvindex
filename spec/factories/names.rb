91c988f1aa16ce6f1fa46e4d141e4fe133ea10fc
Masayuki Ataka <masayuki.ataka@gmail.com>
2014-02-05 13:27:40 +0000

    Wrote tests for name model

--- spec/factories/names.rb
+++ spec/factories/names.rb
@@ -1,7 +1,21 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :bach, class: Name do
-    last 'Bach'
+  factory :no_name, class: Name do
+  end
+
+  factory :strauss, class: Name do
+    last 'Strauss'
+  end
+
+  factory :j_strauss, class: Name do
+    first 'Johann'
+    last 'Strauss'
+  end
+
+  factory :j_strauss2, class: Name do
+    first 'Johann'
+    last  'Strauss'
+    suffix 'II'
   end
 end
--- spec/models/name_spec.rb
+++ spec/models/name_spec.rb
@@ -2,9 +2,34 @@ require 'spec_helper'
 
 describe Name do
 
-  it 'save without error' do
-    bach = FactoryGirl.create(:bach)
-    expect(bach.save).to be_true
+  describe 'Create and Save' do
+    context 'No last name' do
+      it 'error if last name is not present' do
+        I18n.enforce_available_locales = false
+        expect(FactoryGirl.build(:no_name)).to have(1).error_on(:last)
+      end
+    end
+
+    context 'Last name only' do
+      it 'save without error' do
+        strauss = FactoryGirl.create(:strauss)
+        expect(strauss.save).to be_true
+      end
+    end
+
+    context 'First and last name' do
+      it 'save without error' do
+        j_strauss = FactoryGirl.create(:j_strauss)
+        expect(j_strauss.save).to be_true
+      end
+    end
+
+    context 'First and last name with suffix' do
+      it 'save without error' do
+        j_strauss2 = FactoryGirl.create(:j_strauss2)
+        expect(j_strauss2.save).to be_true
+      end
+    end
   end
 
 end
